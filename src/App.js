5ec35f217d36a60f59e7a616c81df6d9170b96b0
knromaric <nzekeng@gmail.com>
2019-02-12 16:00:49 +0200

    Added clarifai API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
 import Rank from './components/Rank/rank';
+import FaceRecognition from './components/FaceRecognition/faceRecognition';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
-
 import './App.css';
 
 const particlesOptions = {
@@ -19,18 +20,33 @@ const particlesOptions = {
     }
   };
 
+const app = new Clarifai.App({
+  apiKey: 'f53e9e87f8bb4319bd4c0ee8b16bfcfc'
+});
 
 class App extends Component {
   state={
-    input: ''
+    input: '', 
+    imageUrl: ''
   }
 
   handleInputChange= (event) => {
-    console.log(event.target.value)
+    this.setState({input:event.target.value});
   };
 
-  handleSubmit = () => {
-    console.log('click');
+  handleButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+       this.state.input)
+       .then(
+      function (response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function (err) {
+        // there was an error
+      }
+    );
   }
 
   render() {
@@ -44,8 +60,8 @@ class App extends Component {
        <Rank />
        <ImageLinkForm 
         onInputChange={this.handleInputChange} 
-        onButtonSubmit={this.handleSubmit} />
-      {/*  <FaceRecognition /> */}
+        onButtonSubmit={this.handleButtonSubmit} />
+       <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
---
+++ src/components/FaceRecognition/faceRecognition.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FaceRecognition = ({ imageUrl }) => {
+  return (
+    <div className="Center ma">
+      <div className='mt2'>
+        <img width='500px' height='auto' src={imageUrl} alt="target" wi />
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
